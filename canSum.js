c4f871e40519977690cf7836b159652220221fa1
Nick Carbone <nfcarbone@gmail.com>
2020-12-10 22:53:59 -0500

    setup sum problems

--- canSum.js
+++ canSum.js
@@ -1,13 +1,6 @@
-// only two operands
-// const canSum = (t, arr) => {
-//   if (arr.length === 1 && arr[0] !== t) return false;
-//   if (arr[0] === t) return true;
-//   const op = arr.shift();
-//   for (let i of arr) {
-//     if (op + i === t) return true;
-//   }
-//   return canSum(t, arr);
-// };
+const canSum = (targetSum, arr) => {};
 
+// can any combination of array elements
+// sum to the targetSum, you may reuse elements
 console.log(canSum(7, [5, 3, 4, 7]));
 console.log(canSum(7, [2, 4]));
---
+++ howSum.js
@@ -0,0 +1,9 @@
+const howSum = (targetSum, arr) => {};
+
+// return any combination of array elements
+// whose sum is the targetSum
+console.log(howSum(7, [2, 3]));
+console.log(howSum(7, [5, 3, 4, 7]));
+console.log(howSum(7, [2, 4]));
+console.log(howSum(8, [2, 3, 5]));
+// console.log(howSum(300, [7, 14]));
