7642c8a0be9783b2a4625485fafb580ef0a6d53c
Nick Carbone <nfcarbone@gmail.com>
2020-12-10 22:42:21 -0500

    nick way no good

--- canSum.js
+++ canSum.js
@@ -1,12 +1,13 @@
-const canSum = (t, arr) => {
-  if (arr.length === 1 && arr[0] !== t) return false;
-  if (arr[0] === t) return true;
-  const op = arr.shift();
-  for (let i of arr) {
-    if (op + i === t) return true;
-  }
-  return canSum(t, arr);
-};
+// only two operands
+// const canSum = (t, arr) => {
+//   if (arr.length === 1 && arr[0] !== t) return false;
+//   if (arr[0] === t) return true;
+//   const op = arr.shift();
+//   for (let i of arr) {
+//     if (op + i === t) return true;
+//   }
+//   return canSum(t, arr);
+// };
 
 console.log(canSum(7, [5, 3, 4, 7]));
 console.log(canSum(7, [2, 4]));
