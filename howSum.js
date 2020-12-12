8685411ef041c71f677c3fc3e7d542aef673d1fe
Nick Carbone <nfcarbone@gmail.com>
2020-12-11 21:48:48 -0500

    howSum memoized

--- howSum.js
+++ howSum.js
@@ -1,4 +1,18 @@
-const howSum = (targetSum, arr) => {};
+const howSum = (targetSum, arr, memo = {}) => {
+  if (targetSum in memo) return memo[targetSum];
+  if (targetSum === 0) return [];
+  if (targetSum < 0) return;
+  for (let num of arr) {
+    const winner = howSum(targetSum - num, arr, memo);
+    if (winner) {
+      winner.push(num);
+      memo[targetSum] = winner;
+      return memo[targetSum];
+    }
+  }
+  memo[targetSum] = null;
+  return;
+};
 
 // return any combination of array elements
 // whose sum is the targetSum
@@ -6,4 +20,4 @@ console.log(howSum(7, [2, 3]));
 console.log(howSum(7, [5, 3, 4, 7]));
 console.log(howSum(7, [2, 4]));
 console.log(howSum(8, [2, 3, 5]));
-// console.log(howSum(300, [7, 14]));
+console.log(howSum(300, [7, 14]));
