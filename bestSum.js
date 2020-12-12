5272d04f5e55f53394659e6292548c13571284b1
Nick Carbone <nfcarbone@gmail.com>
2020-12-11 22:19:45 -0500

    bestSum setup

---
+++ bestSum.js
@@ -0,0 +1,9 @@
+const bestSum = (targetSum, arr, memo = {}) => {};
+
+// return shortest combination of array elements
+// whose sum is the targetSum
+console.log(bestSum(7, [2, 3]));
+console.log(bestSum(7, [5, 3, 4, 7]));
+console.log(bestSum(7, [2, 4]));
+console.log(bestSum(8, [2, 3, 5]));
+console.log(bestSum(300, [7, 14]));
