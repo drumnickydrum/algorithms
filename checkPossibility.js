5a70ac99989d65e2ab1351e29bc1dd028779e144
nfcarbone <nfcarbone@gmail.com>
2020-12-30 21:52:20 -0500

    sane version of checkPossibility

--- checkPossibility.js
+++ checkPossibility.js
@@ -1,40 +1,24 @@
 var checkPossibility = function (nums) {
-  const checkSorted = (array) => {
-    let sorted = [...array];
-    sorted.sort((a, b) => a - b);
-    let isSorted = true;
-    for (let i = 0; i < array.length; i++) {
-      if (array[i] !== sorted[i]) isSorted = false;
+  const checkAscending = () => {
+    let max = -Infinity;
+    let prevMax = max;
+    for (let i = 0, len = nums.length; i < len; i++) {
+      if (nums[i] < max) return [false, i, nums[i], max, prevMax];
+      prevMax = max;
+      max = nums[i];
     }
-    return isSorted;
+    return [true];
   };
-  // if (checkSorted(nums)) return true;
-
-  let flipMax = [...nums];
-  for (let i = 0, prev = -Infinity, prev2 = null; i < nums.length; i++) {
-    if (flipMax[i] < prev) {
-      flipMax[i - 1] = prev2;
-      break;
-    }
-    prev2 = prev;
-    prev = nums[i];
-  }
-  if (checkSorted(flipMax)) return true;
-
-  let flipOffender = [...nums];
-  for (let i = 0, prev = -Infinity; i < nums.length; i++) {
-    if (flipOffender[i] < prev) {
-      flipOffender[i] = prev;
-      break;
-    }
-    prev = nums[i];
-  }
-  if (checkSorted(flipOffender)) return true;
-
+  let [result, offenderIndex, offender, max, prevMax] = checkAscending();
+  if (result) return true;
+  nums[offenderIndex] = max;
+  if (checkAscending()[0]) return true;
+  nums[offenderIndex] = offender;
+  nums[offenderIndex - 1] = prevMax;
+  if (checkAscending()[0]) return true;
   return false;
 };
-// flip max to prev, sort, check
-// flip offender to max, sort, check
+// no sorting... just flip either prev or offender and check again
 
 console.log(checkPossibility([5, 9, 7, 8])); // true / offender: prev
 ///////////////////////////  [5, 5, 7, 8]
@@ -58,5 +42,5 @@ console.log(checkPossibility([5, 7, 2, 1])); // false : descending twice
 console.log(checkPossibility([1, 2, 3, 4, 6, 5, 7, 9, 8])); // false : descending twice
 /////////////////////////////[1, 2, 3, 4, 6, 6, 7, 9, 8]
 console.log(checkPossibility([2, 3, 3, 2, 2])); // false :
-console.log(checkPossibility([3, 4, 2, 3])); // false : sub less than prev max
 console.log(checkPossibility([1, 3, 5, 2, 4])); // false:
+console.log(checkPossibility([3, 4, 2, 3])); // false : descending twice
